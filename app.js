c3c5538cd8e5d203e7a257affea2c432072f7df8
Prish20 <contact@christadriansanya.me>
2025-02-09 00:02:40 +0300

    implement express server with default route and fix package.json formatting

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Server is running on port 3000!");
+import express from 'express';
+
+const app = express();
+
+/**
+ * This is the default route of the application.
+ * It returns a welcome message to the user.
+ */
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+/**
+ * This is the entry point of the application.
+ * It starts the server and listens on port 3000.
+ */
+app.listen(3000, () => {
+  console.log('Subscription Tracker is running on http://localhost:3000');
+});
+
+export default app;

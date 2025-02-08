ccfd53672bcdf2fa8b5482a0544026776e53121b
Prish20 <contact@christadriansanya.me>
2025-02-09 00:18:13 +0300

    add dotenv support for environment variables and update server port configuration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {PORT} from './config/env.js';
 
 const app = express();
 
@@ -12,10 +13,10 @@ app.get('/', (req, res) => {
 
 /**
  * This is the entry point of the application.
- * It starts the server and listens on port 3000.
+ * It listens to port 3000 and logs the message to the console.
  */
-app.listen(3000, () => {
-  console.log('Subscription Tracker is running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker is running on http://localhost:${PORT}`);
 });
 
 export default app;
---
+++ config/env.js
@@ -0,0 +1,18 @@
+import { config } from "dotenv";
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ * This is a good place to load environment variables.
+ * @type {Object}
+ * @see {@link https://www.npmjs.com/package/dotenv|dotenv}
+ */
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+/**
+ * Export environment variables
+ * @type {Object}
+ * @property {string} PORT - Port number
+ * @property {string} NODE_ENV - Environment
+ * @see {@link https://nodejs.org/api/process.html#process_process_env|process.env}
+ */
+export const { PORT, NODE_ENV } = process.env;
